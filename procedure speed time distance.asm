2a6d963202fe0c0bc2d0a99bc6df4057beabbbef
mandem296 <75235392+mandem296@users.noreply.github.com>
2022-02-03 01:18:14 +0300

    using procedures to implement the distance, time and speed formula

---
+++ procedure speed time distance.asm
@@ -0,0 +1,28 @@
+;ut + 1/2at^2
+u=5
+t=2
+a=3
+org 100h
+MOV AX,u
+mov CX,t
+                     
+CALL P1 ;ut
+CALL P2 ;1/2at^2
+CALL P3 ;ut+1/2at^2
+RET
+P1 PROC ;ut
+    mul Cx
+    mov Bx,Ax
+ret      
+P2 PROC ;1/2at^2
+    mov Ax,t
+    mul CX
+    mov CX,a
+    mul CX
+    mov CX,2
+    div CX
+ret    
+P3 PROC ;ut+1/2at^2
+    add Ax,Bx
+    OUT 199,AX                               
+ret
